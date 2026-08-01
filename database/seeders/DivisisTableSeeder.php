ac13b692f5f6059aefbd930e07d38e8c86469907
zen2909 <118924860+zen2909@users.noreply.github.com>
2026-08-02 03:26:09 +0700

    fix Divisi seeder

--- database/seeders/DivisisTableSeeder.php
+++ database/seeders/DivisisTableSeeder.php
@@ -13,7 +13,7 @@ class DivisisTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('divisions')->insert([
+        DB::table('divisis')->insert([
             [
                 'name' => '1',
                 'description' => 'Divisi yang bertugas untuk mengembangkan kemampuan seluruh anggota Ikatan Mahasiswa Arosbaya dalam bidang akademis.',
